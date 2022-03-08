bf5c456cd7691f872d09a9081119e6c0bf18ab84
GiselyFernanda <t991636287@gmail.com>
2022-03-08 11:25:21 -0300

    Alterações

--- aula_02/controle/function.js
+++ aula_02/controle/function.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
 
         let dados = $('#form').serialize()
 
-        console.log(dados)
+       $.ajax({
+            type: 'POST',
+            dataType: 'JSON',
+            assync: true,
+            data: dados,
+            url: '../modelo/retorno.php',
+            success: function(){
+                
+            }
+       })
     })
 })
